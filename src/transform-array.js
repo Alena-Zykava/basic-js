456d33a5cb5748f8a4ebc40004471158bcc045d3
Alena-Zykava <zykava.alena@gmail.com>
2020-11-01 14:39:33 +0300

    :fix: task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,32 +1,42 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
-  if (arr&&arr.length > 0) {
-    let result =[];
-    for (let i = 0; i<arr.length; i++){
-      if (arr[i] === '--discard-next') {
-            if (i == arr.length - 1) {
-              return false;
-          } else {
-            result = arr.splice( i, 2);
-          }
-      } else if (arr[i] === '--discard-prev'){
-            if (i == 0) {
-              return false;
-          } else {
-            result = arr.splice( i-1, 1);
-          }
-      } else if (arr[i] === '--double-next') {
-            if (i == arr.length - 1) {
-              return false;
-          }  else {
-            result = arr.splice(i+1, 0, arr[i+1]);
-          }
+  if (!Array.isArray(arr)) {throw new Error('error');}
+  let result = [];
+  for (let i=0; i<arr.length; i++) {
+    let elem = arr[i];    
+    switch(elem) {
+      case'--discard-prev':
+        if (i-2>=0 && arr[i-2] === '--discard-next') {
+          break;
+        }
+        if (i-1>=0) {
+          result.pop();
+        }
+      break;
+      case'--discard-next':
+      if (i+1<=arr.length-1) {        
+        i++;
       }
-
+      break;
+      case'--double-prev':
+        if (i-2>=0 && arr[i-2] === '--discard-next') {
+          break;
+        }
+        if (i-1 >= 0) {        
+          result.push(result[result.length-1]);        
+        }
+      break;
+      case'--double-next':
+      if (i+1<=arr.length-1) {        
+        result.push(arr[i+1]);
+      }
+      break;
+      default:
+        result.push(elem);
+      break;
     }
-  return result;
   }
-  return false;
+  return result;
 };
 
